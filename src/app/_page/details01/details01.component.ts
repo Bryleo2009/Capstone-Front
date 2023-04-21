dcb07d77903ba3a742fa2411fc291a5f786ca6e3
CarlosS123A <salinasreyesc2002@gmail.com>
2023-04-20 19:21:54 -0500

    Detalle01

---
+++ src/app/_page/details01/details01.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details01',
+  templateUrl: './details01.component.html',
+  styleUrls: ['./details01.component.css']
+})
+export class Details01Component {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { PrimeNGModule } from './prime-ng.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Details01Component } from './_page/details01/details01.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Details01Component
   ],
   imports: [
     BrowserModule,
