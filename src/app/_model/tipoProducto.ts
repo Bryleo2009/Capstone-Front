e5fbbf37a5bacb700597a17746e6cade9336094b
CarlosS123A <salinasreyesc2002@gmail.com>
2023-06-21 16:38:30 -0500

    AvanceCRUDSEMIOK02

--- src/app/_model/tipoProducto.ts
+++ src/app/_model/tipoProducto.ts
@@ -3,6 +3,10 @@ import { Enum } from "./enum";
 export class TipoProducto {
 
     idTipoProduc!: number;
+    abreviItem!: string;
+    identItem!: string;
+    nombreItem!: string;
+    vistaItem!: string;
     //ese string iba antes de poner la funcion de listar agregar ccrud tiporproducto
     /*nombreTipoProduc!: string;*/
 }
